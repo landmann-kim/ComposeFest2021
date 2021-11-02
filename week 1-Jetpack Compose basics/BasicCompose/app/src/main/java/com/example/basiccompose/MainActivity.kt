8f946b292a392d3d4c538201b96ef6d7dd0d5ed2
jaydenkim <jayden.kim@kakaocorp.com>
2021-11-02 14:29:06 +0900

    [Week1] Jetpack Compose Basic sample project 추가
    - BasicCompose

---
+++ week 1-Jetpack Compose basics/BasicCompose/app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.basiccompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basiccompose.ui.theme.BasicComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicComposeTheme {
+        Greeting("Android")
+    }
+}
